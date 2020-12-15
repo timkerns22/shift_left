16079e3885c1784ec706e9b897b1373807a146d6
timkerns22 <73653468+timkerns22@users.noreply.github.com>
2020-12-15 11:37:01 -0500

    Add files via upload

---
+++ Violation-EBSVolume.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-1"
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-west-2a"
+  size              = 40
+
+  encrypted			= false
+}
+
+resource "aws_instance" "web" {
+  ami               = "ami-21f78e11"
+  availability_zone = "us-west-2a"
+  instance_type     = "t1.micro"
+}
